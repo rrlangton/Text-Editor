d3f6fd0af3458584e4c87776d642a66ec4f4a9da
Robin <rrlangton@gmail.com>
2024-04-27 23:24:20 -0500

    eslintrc added, database.js updated

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,66 +1,42 @@
-import { postDb, getDb, deleteDb } from './database';
+// Import methods to save and get data from the indexedDB database in './database.js'
+import { getDb, putDb } from './database';
+import { header } from './header';
 
-const form = document.getElementById('contact-form');
+export default class {
+  constructor() {
+    const localData = localStorage.getItem('content');
 
-// Adds deleteCard() to the global scope so each card has access to it.
-window.deleteCard = (e) => {
-// Grabs the id from the button element attached to the contact card.
-  let id = parseInt(e.id);
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
 
-  // Delete the card
-  deleteDb(id);
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
 
-  // Reload the DOM
-  fetchCards();
-};
+    // When the editor is ready, set the value to whatever is stored in indexeddb.
+    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    getDb().then((data) => {
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    });
 
-form.addEventListener('submit', (event) => {
-  // handle the form data
-  event.preventDefault();
-  const name = form.elements['name'].value;
-  const home = form.elements['home-phone'].value;
-  const cell = form.elements['cell-phone'].value;
-  const email = form.elements['email'].value;
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
 
-  // Post form data to IndexedDB
-  postDb(name, home, cell, email);
-
-  // Submit the form
-  form.reset();
-
-  // Reload the DOM
-  fetchCards();
-});
-
-const fetchCards = async () => {
-// Grab card data from IndexedDB
-  const result = await getDb();
-
-  let card = ` `;
-
-  // Loop through the data and create the contact card
-  for (let data of result) {
-    console.log(data);
-    card += `
-    <div class="card card-rounded col-md-3 m-2">
-      <div class="card-header card-rounded">
-        <h1>${data.name}</h1>
-      </div>
-      <div class="card-body">
-        <p>Home Phone: ${data.home_phone}</p>
-        <p>Cell Phone: ${data.cell_phone}</p>
-        <p>Email: ${data.email}</p>
-      </div>
-      <div class="flex-row justify-flex-end p-1">
-        <button class="btn btn-sm btn-danger" id="${data.id}" onclick="deleteCard(this)">Delete</button>
-      </div>
-    </div>
-    `;
+    // Save the content of the editor when the editor itself is loses focus
+    this.editor.on('blur', () => {
+      console.log('The editor has lost focus');
+      putDb(localStorage.getItem('content'));
+    });
   }
-
-  // Setting innerHTML as card variable
-  document.getElementById('card-group').innerHTML = card;
-};
-
-// Fetch cards upon being loaded.
-fetchCards();
+}
